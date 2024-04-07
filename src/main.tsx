a203858b5cd250c8867c1029a789e68fd18e3a40
Lond Treeforce <londtreeforce@gmail.com>
2024-04-07 09:38:43 +0300

    Add tonconnect manifest

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,16 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-// this manifest is used temporarily for development purposes
-// const manifestUrl =
-//   "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+//this manifest is used temporarily for development purposes
+const manifestUrl =
+  "URL";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <TonConnectUIProvider manifestUrl={manifestUrl}>
-  <TonConnectUIProvider >
+   <TonConnectUIProvider manifestUrl={manifestUrl}>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
